3bbe3b9b97e8bff2d492fac13a9e4367325753b3
choehn86 <42713647+choehn86@users.noreply.github.com>
2018-08-26 10:28:25 -0400

    Update getadinfo.ps1
    - Renamed 'copyattribute' to 'modifier' and changed code handling accordingly
    - Utilized 'Check-Command' function to verify if Get-ADUser is installed
    - Added additional config file validation

--- getadinfo.ps1
+++ getadinfo.ps1
@@ -1,7 +1,26 @@
-﻿[cmdletbinding()]
+<#
+.SYNOPSIS
+    Uses Get-ADUser to display AD user attributes in a 'pretty' way for the command line.
+.DESCRIPTION
+    Requires the supplied 'config.json' file for parsing domain controllers (optional), user attributes/properties (mandatory),
+    aliases (optional), group membership modifications (optional), and 'modifiers' (optional - example provided is flag to copy an attribute to the clipboard).
+    'config.json' must be placed in the same working dir as the script to function.
+.PARAMETER Path
+    The path to the .
+.EXAMPLE
+    .\getadinfo.ps1 USER001 c
+    .\getadinfo.ps1 USER002
+.LINK
+    https://github.com/choehn86/getadinfo
+#>
+
+[cmdletbinding()]
 param (
-	[string]$userID,
-    	[char] $modifer
+	#samAccountName to query against AD (required)
+    [Parameter(Mandatory=$true)]
+    [string]$userID,
+    #generic modifier - currently used to copy a specified attribute from the config file to the clipboard (optional)
+    [char] $modifer
 )
 
 if ($PSBoundParameters['Debug']) 
@@ -12,7 +31,8 @@ if ($PSBoundParameters['Debug'])
 # Load and parse the JSON configuration file
 try 
 {
-	$config = Get-Content -Path .\config.json -Raw -ErrorAction:SilentlyContinue -WarningAction:SilentlyContinue | ConvertFrom-Json -ErrorAction:SilentlyContinue -WarningAction:SilentlyContinue
+	$config = Get-Content -Path .\config.json -Raw -ErrorAction:SilentlyContinue -WarningAction:SilentlyContinue | 
+        ConvertFrom-Json -ErrorAction:SilentlyContinue -WarningAction:SilentlyContinue
 } 
 catch 
 {
@@ -22,15 +42,15 @@ catch
 # Check configuration file; if valid then assign values to global vars
 if (!($config)) 
 {
-	Write-Error -Message "The Base configuration file is missing!" -ErrorAction Stop
+	Write-Error -Message "No data found in config file!" -ErrorAction Stop
 }
 else
 {
-    $global:DCs = ($config.getadinfo.domaincontrollers)
-    $global:props = ($config.getadinfo.properties.props)
-    $global:aliases = ($config.getadinfo.properties.aliases)
-    $global:groupformatting = ($config.getadinfo.groupformatting)
-    $global:copyattribute = ($config.getadinfo.copyattribute)
+    if( ($global:DCs = $config.getadinfo.domaincontrollers).length -eq 0 ) { Write-Debug "no DCs parsed from file, resorting to default logon server" }     
+    if( ($global:props = $config.getadinfo.properties.props).length -eq 0 ) { Write-Error -Message "No properties parsed, check config file!" -ErrorAction Stop }
+    if( ($global:aliases = $config.getadinfo.properties.aliases).length -eq 0 ) { Write-Debug "no aliases parsed from file" }
+    if( ($global:groupformatting = $config.getadinfo.groupformatting).length -eq 0 ) { Write-Debug "no group formating parsed from file" }
+    if( ($global:modifier = $config.getadinfo.modifier).length -eq 0 ) { Write-Debug "no modifiers parsed from file" }
 }
 
 function Check-Command($cmdname) # checks if cmdlet is loaded/installed
@@ -38,74 +58,88 @@ function Check-Command($cmdname) # checks if cmdlet is loaded/installed
     return [bool](Get-Command -Name $cmdname -ErrorAction SilentlyContinue)
 }
 
-Write-Debug "Using Get-ADUser cmdlet..."
-try
+if(Check-Command('Get-ADUser'))
 {
-    $c = 0
-    $user = $null
-    $groups = $null
-    while($c -le [int]$global:DCs.Count)
+    try
     {
-        try
+        Write-Debug "Using Get-ADUser cmdlet..."
+        $c = 0
+        $user = $null
+        $groups = $null
+        while($c -le [int]$global:DCs.Count)
         {
-            if($c -lt [int]$global:DCs.Count)
+            try
             {
-                $user = Get-ADUser $userID -Server $global:DCs[$c] -Properties * | Select -Property $global:props
-                break;
-            }
-            else
-            {
-                $user = Get-ADUser $userID -Properties * | Select -Property $global:props
-                break;
-            }
-        }
-        catch [Microsoft.ActiveDirectory.Management.ADServerDownException]
-        {
-            switch (([int]$global:DCs.Count - $c))
-            {
-                0 { $errorstr = "Unable to reach $($env:LOGONSERVER.Replace('\','')  + "." + $env:USERDNSDOMAIN)" }
-                1 { $errorstr = "Unable to reach $($global:DCs[$c]), using $($env:LOGONSERVER.Replace('\','')  + "." + $env:USERDNSDOMAIN)" }
-                default {  $errorstr = "Unable to reach $($global:DCs[$c]),trying next DC in config file..." }
-            }
-            Write-Debug $errorstr
-            $c++
-        }
-    }
-
-    if($user)
-    {
-        $user | Add-Member -MemberType NoteProperty -Name objectGUID -Value (($user.ObjectGUID.ToByteArray() | foreach { $_.ToString("X2") }) -join '' ) -Force
-        $global:aliases | Foreach-Object { Add-Member -InputObject $user -MemberType AliasProperty -Name $_.name -Value $_.prop -Force }
-     
-        # copy attribute to clipboard
-        if($modifer -eq 'c')
-        {
-            $user.($global:copyattribute.name).Replace($global:copyattribute.omit,"") | Set-Clipboard
-            Write-Debug "copied $($global:copyattribute.name) to clipboard"
-        }
-
-        $user | Select -Property * -ExcludeProperty $($global:aliases.prop + ("memberOf"))
-
-        if($user.memberOf.length -gt 0)
-        {
-            [int]$counter = 1
-            $groups = $user.memberOf.replace("CN=", "") | Sort-Object { ($_ -match "$($global:groupformatting.filter -join '|')") } -Descending
-            
-            "------------------  Member Of ------------------"           
-            foreach($group in $groups)
-            {
-                $groupColor = "cyan"
-                foreach($fmt in $global:groupformatting)
+                if($c -lt [int]$global:DCs.Count)
                 {
-                    if( $group -match $fmt.filter ) { $groupColor = $fmt.color }
+                    $user = Get-ADUser $userID -Server $global:DCs[$c] -Properties * | Select -Property $global:props
+                    break;
                 }
-                Write-Host ('' + $counter + ' >> ' + $group) -foregroundColor $groupColor
-                $counter++  
+                else
+                {
+                    $user = Get-ADUser $userID -Properties * | Select -Property $global:props
+                    break;
+                }
             }
-            "------------------------------------------------"
+            catch [Microsoft.ActiveDirectory.Management.ADServerDownException]
+            {
+                switch (([int]$global:DCs.Count - $c))
+                {
+                    0 { $errorstr = "Unable to reach $($env:LOGONSERVER.Replace('\','')  + "." + $env:USERDNSDOMAIN)" }
+                    1 { $errorstr = "Unable to reach $($global:DCs[$c]), using $($env:LOGONSERVER.Replace('\','')  + "." + $env:USERDNSDOMAIN)" }
+                    default {  $errorstr = "Unable to reach $($global:DCs[$c]),trying next DC in config file..." }
+                }
+                Write-Debug $errorstr
+                $c++
+            }
         }
-        else { "User is not a member of any groups!" }
-    } 
-    else { "No data found" }
-} 
-catch { $_  }
+
+        if($user)
+        {
+            $user | Add-Member -MemberType NoteProperty -Name objectGUID -Value (($user.ObjectGUID.ToByteArray() | foreach { $_.ToString("X2") }) -join '' ) -Force
+            $global:aliases | Foreach-Object { Add-Member -InputObject $user -MemberType AliasProperty -Name $_.name -Value $_.prop -Force }
+     
+            # handle any supplied modifiers
+            foreach($gmod in $global:modifier)
+            {    
+                switch($modifer)
+                {
+                    'c' # copy attribute to clipboard
+                        {
+                            if($gmod.action -eq 'copy')
+                            {
+                                $user.($gmod.name).Replace([string]$gmod.omit,"") | Set-Clipboard
+                                Write-Debug "copied $($gmod.name) to clipboard" 
+                            }
+                        }
+                    default { Write-Debug "no modifiers included" }         
+                }
+            }
+
+            $user | Select -Property * -ExcludeProperty $($global:aliases.prop + ("memberOf"))
+
+            if($user.memberOf.length -gt 0)
+            {
+                [int]$counter = 1
+                $groups = $user.memberOf.replace("CN=", "") | Sort-Object { ($_ -match "$($global:groupformatting.filter -join '|')") } -Descending
+            
+                "------------------  Member Of ------------------"           
+                foreach($group in $groups)
+                {
+                    $groupColor = "cyan"
+                    foreach($fmt in $global:groupformatting)
+                    {
+                        if( $group -match $fmt.filter ) { $groupColor = $fmt.color }
+                    }
+                    Write-Host ('' + $counter + ' >> ' + $group) -foregroundColor $groupColor
+                    $counter++  
+                }
+                "------------------------------------------------"
+            }
+            else { "User is not a member of any groups!" }
+        } 
+        else { "No data found" }
+    }
+    catch { $_  }
+}
+else { 'Get-ADUser cmdlet not found!' }
